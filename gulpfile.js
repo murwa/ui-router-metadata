edfc55708fff0bada81e95e5184299cf467831ea
Peter Murwa <wanjala.pet@gmail.com>
2017-06-11 22:09:17 +0300

    Fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ var uglify = require('gulp-uglify'),
 
 gulp.task('default', function () {
     gulp.src(['src/**/*.js'])
-        .pipe(concat())
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('dist/'))
         .pipe(uglify())
         .pipe(rename({ basename: "ui-router-metadata", suffix: ".min", extname: ".js" }))
         .pipe(gulp.dest('dist/'));
