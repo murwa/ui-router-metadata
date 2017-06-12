5ab604ce40b3d617439452635beb3f0c0eb15691
Peter Murwa <wanjala.pet@gmail.com>
2017-06-12 12:13:33 +0300

    Use  service

--- dist/main.js
+++ dist/main.js
@@ -71,14 +71,14 @@
                 }
             }
         }])
-        .run(['$metadata', '$rootScope', '$timeout', function ($metadata, $rootScope, $timeout) {
+        .run(['$metadata', '$rootScope', '$timeout', '$window', function ($metadata, $rootScope, $timeout, $window) {
             console.log('Binding event listeners')
             $rootScope.$on('$stateChangeSuccess', function () {
                 console.log('State changed')
                 $timeout(function () {
                     console.log($metadata.getTitle());
                     console.log('Title set - ' + $metadata.getTitle());
-                    window.title = $metadata.getTitle();
+                    $window.title = $metadata.getTitle();
                 });
             });
         }]);
